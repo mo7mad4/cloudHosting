aec593062261ea8a09689b362cbb60047150c34a
mo7mad4 <mohammedreda814@gmail.com>
2024-06-05 22:56:12 +0200

    Adding some code and adding some icon make open menu and close menu

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,30 +1,57 @@
+"use client";
 import Link from 'next/link'
-import React from 'react'
-import { GrTechnology } from 'react-icons/gr'
+import React, { useState } from 'react'
+
+//style
 import styles from './header.module.css'
+
+// icons
+import { GrTechnology } from 'react-icons/gr'
+import { AiOutlineMenu } from "react-icons/ai";
+import { IoMdClose } from "react-icons/io";
+
+
 
 
 const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-        <Navbar />
-    <div>
-        <Link href="/" className={styles.logo}>
-            CLOUD
-            <GrTechnology />
-            HOSTING
-        </Link>
-    </div>
+    const [toggle, setToggle] = useState(false);
 
-    <ul className={styles.navLinks}>
-        <Link className={styles.navLink} href="/">Home</Link>
-        <Link className={styles.navLink} href="/articles">Articles</Link>
-        <Link className={styles.navLink} href="/about">About</Link>
-        <Link className={styles.navLink} href="/admin">Admin Dashboard</Link>
-    </ul>
+    return (
+        <nav className={styles.navbar}>
+            <div>
+                <Link href="/" className={styles.logo}>
+                    CLOUD
+                    <GrTechnology />
+                    HOSTING
+                </Link>
+                <div className={styles.menu}>
+                    {
+                        toggle
+                            ?
+                            <IoMdClose
+                                onClick={() => setToggle(prev => !prev)} />
+                            :
+                            <AiOutlineMenu
+                                onClick={() => setToggle(prev => !prev)}
+                            />}
 
-</nav>
-  )
+                </div>
+            </div>
+            <div
+                className={styles.navLinksWrapper}
+                style={{
+                    clipPath: toggle && "polygon(0 0, 100% 0, 100% 100%, 0 100%)" || ""
+                }}
+            >
+                <ul className={styles.navLinks}>
+                    <Link className={styles.navLink} onClick={() => setToggle(false)} href="/">Home</Link>
+                    <Link className={styles.navLink} onClick={() => setToggle(false)} href="/articles">Articles</Link>
+                    <Link className={styles.navLink} onClick={() => setToggle(false)} href="/about">About</Link>
+                    <Link className={styles.navLink} onClick={() => setToggle(false)} href="/admin">Admin Dashboard</Link>
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
